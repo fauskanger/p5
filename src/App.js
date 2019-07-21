bee2c14359a471730a1826ec117bc402b50f5fde
Thomas Fauskanger <thomasfauskanger@gmail.com>
2019-07-21 17:24:58 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+
+// import { demoSketch } from './p5';
+import { mandelbrotSketch } from './p5/mandelbrot';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  p5Element = null;
+  state = {
+      rotation: 45
+  };
+  render = () => {
+    return (
+        <div className="App">
+          <header className="App-header">
+              Mandelbrot Set Visualization
+           {/*Set a number ({ this.state.rotation }°): */}
+              {/*<input type={"range"} min={-360} max={360} value={this.state.rotation} onChange={(e) => this.setState({rotation: Number(e.target.value)})}/>*/}
+          </header>
+          <div ref={e => this.p5Element = e} style={{
+            display: 'flex',
+            flexGrow: 1,
+          }}>
+            <P5Wrapper sketch={mandelbrotSketch} rotation={this.state.rotation} />
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/p5/mandelbrot.js
@@ -0,0 +1,103 @@
+import * as M from 'mathjs';
+
+export const mandelbrotSketch = p => {
+
+    let lessThanInfinity = 100;
+    let maxIterations = 200;
+    let colorOffset = 0;
+    let colorEaseExponent = 2/3;
+    let easedMaxIterations = M.pow(maxIterations, colorEaseExponent);
+    let sqrtMaxIterations = M.sqrt(maxIterations);
+
+
+    let imBounds = p.createVector(-1.2, 1.2);
+    let reBounds = undefined;
+    let reMidPoint = -0.5;
+
+    let pixels = [];
+
+    const allPixelsCoordsGenerator = function* () {
+        for (let row = 0; row < p.height; row++) {
+            for (let col = 0; col < p.width; col++) {
+                yield pixels[row][col];
+            }
+        }
+    };
+
+    const mapToBounds = (col, row) => {
+        return {
+            re: p.map(col, 0, p.width, reBounds.x, reBounds.y),
+            im: p.map(row, 0, p.height, imBounds.x, imBounds.y)
+        }
+    };
+
+    const initPixelGrid = () => {
+        for (let row = 0; row < p.height; row++) {
+            pixels[row] = new Array(p.width);
+            for (let col = 0; col < p.width; col++) {
+                const { re, im } = mapToBounds(col, row);
+                const nIterations = getIterationCount(re, im);
+                // const color = p.map(M.sqrt(nIterations), 0, sqrtMaxIterations, 0, 255);
+                const color = p.map(M.pow(nIterations, colorEaseExponent), 0, easedMaxIterations, 0, 255);
+                pixels[row][col] = {
+                    col, row,
+                    re, im,
+                    nIterations,
+                    color
+                };
+            }
+        }
+    };
+
+    const getIterationCount = (cRe, cIm) => {
+        let c = M.complex(cRe, cIm);
+        let z = M.complex(0, 0);
+        for (let i = 0; i < maxIterations; i++) {
+            z = z.pow(2).add(c);
+            if (M.square(z.re) + M.square(z.im) > lessThanInfinity) {
+            // if (z.re * z.re + z.im * z.im > 100) {
+                return i;
+            }
+        }
+        return maxIterations;
+    };
+
+    const initBounds = () => {
+        // Must be called after p.createCanvas for p.height/width to have been set correctly
+        const dimRatio = p.width / p.height;
+        const reWidth = (imBounds.y - imBounds.x) * dimRatio;
+        reBounds = p.createVector(reMidPoint - reWidth / 2, reMidPoint + reWidth / 2);
+
+        console.log('Re axis: ', reBounds.x, ' - ', reBounds.y);
+        console.log('Im axis: ', imBounds.x, ' - ', imBounds.y);
+        console.log('Pixel dimensions: ', window.innerWidth, 'x', window.innerHeight-100);
+    };
+
+    p.setup = function () {
+        p.createCanvas(window.innerWidth, window.innerHeight-100, p.P2D);
+        p.frameRate(0.1);
+        p.angleMode(p.DEGREES);
+        p.background(0);
+        p.rectMode(p.CENTER);
+        p.colorMode(p.HSB);
+        initBounds();
+        initPixelGrid();
+    };
+
+    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+        if (props.maxIterations){
+            maxIterations = props.maxIterations;
+            console.log('Set maxIterations: ' + maxIterations);
+        }
+    };
+
+    p.draw = function () {
+        p.background(0);
+
+        for (const { col, row, color } of allPixelsCoordsGenerator()) {
+            const offsetColor = (color + colorOffset) % 255;
+            p.stroke(offsetColor, p.map(color, 0, 255, 100, 200), 255-color);
+            p.point(col, row);
+        }
+    };
+};
