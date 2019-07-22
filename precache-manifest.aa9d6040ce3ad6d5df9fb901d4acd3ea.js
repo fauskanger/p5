0d0255780340357101725fc4b4e1bab81d0c9bdd
Thomas Fauskanger <thomasfauskanger@gmail.com>
2019-07-22 06:35:07 +0200

    Updates

---
+++ precache-manifest.aa9d6040ce3ad6d5df9fb901d4acd3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f13681f5c203ad95e0cd7dc22e03ea",
+    "url": "/p5/index.html"
+  },
+  {
+    "revision": "33ecda36e820b1267b2f",
+    "url": "/p5/static/css/main.017dcae1.chunk.css"
+  },
+  {
+    "revision": "f54da67f3de2cc578259",
+    "url": "/p5/static/js/2.1fade875.chunk.js"
+  },
+  {
+    "revision": "33ecda36e820b1267b2f",
+    "url": "/p5/static/js/main.480631d5.chunk.js"
+  },
+  {
+    "revision": "61847327cb180616da1f",
+    "url": "/p5/static/js/runtime~main.e312079a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/p5/precache-manifest.518314a02751b0689be9041837d54d83.js"
+  "/p5/precache-manifest.aa9d6040ce3ad6d5df9fb901d4acd3ea.js"
 );
 
 self.addEventListener('message', (event) => {
